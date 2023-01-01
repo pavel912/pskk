820cced3480f00a74027ae4a08b7739492d4527d
pavel912 <gta4thebest@mail.ru>
2023-01-01 17:08:41 +0300

    refactor userpage path and app methods

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, abort, make_response
 from User import User
 from LoginHandler import LoginHandler
-
+from DataValidator import DataValidator
 
 app = Flask("App")
 loginHandler = LoginHandler()
@@ -20,11 +20,11 @@ def login_get():
 
 @app.post('/login')
 def login_post():
-    user = User(request.form["username"], request.form["password"])
-    if loginHandler.login(user):
-        return redirect(f"/{user.username}")
+    user = loginHandler.get_user_by_username_and_password(User(request.form["username"], request.form["password"]))
+    if not user:
+        return make_response(render_template('login.html', flash_message="Incorrect login or password"), 401)
 
-    return render_template('login.html', flash_message="Incorrect login or password")
+    return redirect(f"/user/{user.id}")
 
 
 @app.get('/create')
@@ -34,14 +34,34 @@ def create_user_get():
 
 @app.post('/create')
 def create_user_post():
-    user = User(request.form["username"], request.form["password"], request.form["email"])
-    if not loginHandler.is_user_exists(user):
-        loginHandler.create_user(user)
-        return redirect(f"/login")
+    validator = DataValidator()
 
-    return render_template('create.html', flash_message="User with this username and password already exists")
+    user_data = User(request.form["username"],
+        request.form["password"],
+        request.form["email"],
+        request.form["name"],
+        request.form["surname"],
+        request.form["fathers_name"],
+        validator.convert_string_to_date(request.form["date_of_birth"]),
+        request.form["job_role"],
+        request.form["company_name"])
+
+    error_messages = validator.validate_user_data(user_data)
+
+    user = loginHandler.get_user_by_username_and_password(user_data)
+
+    if user:
+        error_messages.append("User with this username and password already exists")
+    elif request.form["password"] != request.form["confirm_password"]:
+        error_messages.append("Passwords do not match")
+
+    if error_messages:
+        return make_response(render_template('create.html', error_messages=error_messages, user_data=user_data), 401)
+
+    loginHandler.create_user(user_data)
+    return redirect("/login")
 
 
-@app.get("/<username>")
-def user_page_get(username):
-    return render_template('userpage.html', username=username)
+@app.get("/user/<id>")
+def user_page_get(id):
+    return render_template('userpage.html', user=loginHandler.get_user_by_id(id))
