4414b1defa3bfc17d7746bb38fcd066e44a582d2
pavel912 <gta4thebest@mail.ru>
2023-01-01 17:07:45 +0300

    add new user features

--- User.py
+++ User.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column
-from sqlalchemy import Integer
-from sqlalchemy import String
+from sqlalchemy import Column, Integer, String, DATE
 from sqlalchemy.orm import declarative_base
+import datetime as dt
 
 Base = declarative_base()
 
@@ -17,13 +16,29 @@ class User(Base):
 
     email = Column(String)
 
+    name = Column(String)
+
+    surname = Column(String)
+
+    fathers_name = Column(String)
+
+    date_of_birth = Column(DATE)
+
+    job_role = Column(String)
+
+    company_name = Column(String)
+
     def __repr__(self):
         ...
         return f"User(id={self.id!r}, name={self.username!r}, fullname={self.email!r})"
 
-    def __init__(self, username: str, password: str, email: str = ""):
+    def __init__(self, username: str, password: str, email: str = '', name: str = '', surname: str = '', fathers_name: str = '', date_of_birth: dt.date = dt.date(1, 1, 1), job_role: str = '', company_name: str = ''):
         self.username = username
         self.password = password
         self.email = email
-
-
+        self.name = name
+        self.surname = surname
+        self.fathers_name = fathers_name
+        self.date_of_birth = date_of_birth
+        self.job_role = job_role
+        self.company_name = company_name
