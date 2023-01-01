e842fb29a7585f89342efdfcf077589643d0f01b
pavel912 <gta4thebest@mail.ru>
2023-01-01 17:06:49 +0300

    refactor class methods

--- LoginHandler.py
+++ LoginHandler.py
@@ -1,20 +1,20 @@
 from User import User, Base
 from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, Query
 
 
 class LoginHandler:
     def __init__(self):
         self.engine = create_engine("sqlite:///C:\\Users\\lob55\\PycharmProjects\\pythonProject\\db\\pskk_db", echo=True, future=True)
+
+        session = Session(self.engine)
+        session.execute("DROP TABLE IF EXISTS user;")
+        session.commit()
+        session.close()
+
         Base.metadata.create_all(self.engine)
 
-    def login(self, user: User) -> bool:
-        return self.is_user_exists(user)
-
     def create_user(self, user: User) -> bool:
-        if self.is_user_exists(user):
-            return False
-
         session = Session(self.engine)
 
         session.add(user)
@@ -24,26 +24,36 @@ class LoginHandler:
 
         return True
 
-    def is_user_exists(self, user: User) -> bool:
+    def get_user_by_username_and_password(self, user: User) -> User:
+
         session = Session(self.engine)
 
         query = select(User).where((User.username == user.username) & (User.password == user.password))
 
-        user = session.execute(query).one_or_none()
+        user_return = session.execute(query).one_or_none()
 
         session.close()
 
-        if user:
-            return True
+        if user_return:
+            return user_return[0]
 
-        return False
+        return user_return
+    
+    def get_user_by_id(self, id: int) -> User:
+        session = Session(self.engine)
+
+        query = select(User).where(User.id == id)
+
+        user_return = session.execute(query).one_or_none()
+
+        session.close()
+
+        if user_return:
+            return user_return[0]
+
+        return user_return
+
 
     def add_test_users(self):
         user = User("admin", "admin")
-        if not self.is_user_exists(user):
-            session = Session(self.engine)
-            session.add(user)
-
-            session.commit()
-            session.close()
-
+        self.create_user(user)
