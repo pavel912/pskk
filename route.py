c4a726439e3bd4576f5e284c7374ea704af31b27
pavel912 <gta4thebest@mail.ru>
2023-02-17 00:16:46 +0300

    add crud templates for all entities

--- route.py
+++ route.py
@@ -24,18 +24,39 @@ with app.app_context():
     db.session.commit()
 
 
+# main page
 @app.route('/', methods=["GET"])
 def index():
     news = db.session.execute(db.select(News).order_by(News.created_at.desc()).limit(3)).scalars()
     return render_template('index.html', news=news, bigheader=True)
 
 
+# news
 @app.route('/news/<id>/', methods=["GET"])
-def get_news(id):
-    news = db.get_or_404(News, id)
+def get_news_page(id):
+    news = db.get_or_404(News, int(id))
     return render_template('news.html', news=news)
 
 
+# not implemented
+@app.route('/news/create', methods=["GET", "POST"])
+def create_news():
+    pass
+
+
+# not implemented
+@app.route('/news/<id>/update', methods=["GET", "POST"])
+def update_news(id):
+    pass
+
+
+# not implemented
+@app.route('/news/<id>/delete', methods=["POST"])
+def delete_news(id):
+    pass
+
+
+# login
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -56,14 +77,24 @@ def logout():
     return redirect('/login')
 
 
-@app.route('/create_user_account', methods=["GET", "POST"])
+# user
+@app.route("/user/id/<id>", methods=["GET"])
+def get_user_page(id):
+    if not is_exists_user_session(id):
+        return redirect("/login")
+
+    user = db.get_or_404(User, id)
+
+    return render_template('userpage.html', user=user)
+
+
+@app.route('/user/create', methods=["GET", "POST"])
 def create_user():
     if request.method == "POST":
         error_messages = []
 
-        if db.session.execute(db.select(User).where(User.username == request.form["username"])).first() or\
-                db.session.execute(db.select(User).where(User.password == request.form["password"])).first():
-            error_messages.append("User with this username or password already exists")
+        if db.session.execute(db.select(User).where(User.username == request.form["username"])).first():
+            error_messages.append("User with this username already exists")
         elif request.form["password"] != request.form["confirm_password"]:
             error_messages.append("Passwords do not match")
 
@@ -89,26 +120,6 @@ def create_user():
         return redirect("/login")
 
     return render_template('create_user_account.html')
-
-
-def is_exists_user_session(id):
-    if "user_id" not in session:
-        return False
-
-    if session["user_id"] != int(id):
-        return False
-
-    return True
-
-
-@app.route("/user/id/<id>", methods=["GET"])
-def user_page(id):
-    if not is_exists_user_session(id):
-        return redirect("/login")
-
-    user = db.get_or_404(User, id)
-
-    return render_template('userpage.html', user=user)
 
 
 @app.route("/user/id/<id>/update", methods=["GET", "POST"])
@@ -180,8 +191,30 @@ def update_user(id):
     return render_template('update_userdata.html', user=user)
 
 
+# not implemented
+@app.route("/user/id/<id>/delete", methods=["POST"])
+def delete_user(id):
+    pass
+
+
+@app.route("/user/id/<id>/skills", methods=["GET"])
+def get_users_skills_page(id):
+    if not is_exists_user_session(id):
+        return redirect("/login")
+
+    user = db.get_or_404(User, id)
+
+    return render_template('projects.html', user_id=id, projects=user.skills)
+
+
+# not implemented
+@app.route("/user/id/<id>/requests", methods=["GET"])
+def get_users_requests_page(id):
+    pass
+
+
 @app.route("/user/id/<id>/projects", methods=["GET"])
-def get_users_projects(id):
+def get_users_projects_page(id):
     if not is_exists_user_session(id):
         return redirect("/login")
 
@@ -189,3 +222,138 @@ def get_users_projects(id):
 
     return render_template('projects.html', user_id=id, projects=user.projects_participated)
 
+
+# skills
+# not implemented
+@app.route("/user/id/<id>/skill/<skill_id>", methods=["GET"])
+def get_skill_page(id, skill_id):
+    pass
+
+
+# not implemented
+@app.route("/user/id/<id>/skill/create", methods=["GET", "POST"])
+def create_skill(id):
+    pass
+
+
+# not implemented
+@app.route("/user/id/<id>/skill/<skill_id>/update", methods=["GET", "POST"])
+def update_skill(id, skill_id):
+    pass
+
+
+# not implemented
+@app.route("/user/id/<id>/skill/<skill_id>/delete", methods=["POST"])
+def delete_skill(id, skill_id):
+    pass
+
+
+# request
+# not implemented
+@app.route("/user/id/<id>/request/<request_id>", methods=["GET"])
+def get_request_page(id, request_id):
+    pass
+
+
+# not implemented
+@app.route("/user/id/<id>/request/create", methods=["GET", "POST"])
+def create_request(id):
+    pass
+
+
+# not implemented
+@app.route("/user/id/<id>/request/<request_id>/update", methods=["GET", "POST"])
+def update_request(id, request_id):
+    pass
+
+
+# not implemented
+@app.route("/user/id/<id>/request/<request_id>/delete", methods=["POST"])
+def delete_request(id, request_id):
+    pass
+
+
+# project
+# not implemented
+@app.route("/project/id/<id>", methods=["GET"])
+def get_project_page(id):
+    pass
+
+
+# not implemented
+@app.route("/project/create", methods=["GET", "POST"])
+def create_project():
+    pass
+
+
+# not implemented
+@app.route("/project/id/<id>/update", methods=["GET", "POST"])
+def update_project(id):
+    pass
+
+
+# not implemented
+@app.route("/project/id/<id>/delete", methods=["POST"])
+def delete_project(id):
+    pass
+
+
+# project status
+# not implemented
+@app.route("/project_status/id/<id>", methods=["GET"])
+def get_project_status_page(id):
+    pass
+
+
+# not implemented
+@app.route("/project_status/create", methods=["GET", "POST"])
+def create_project_status():
+    pass
+
+
+# not implemented
+@app.route("/project_status/id/<id>/update", methods=["GET", "POST"])
+def update_project_status(id):
+    pass
+
+
+# not implemented
+@app.route("/project_status/id/<id>/delete", methods=["POST"])
+def delete_project_status(id):
+    pass
+
+
+# company
+# not implemented
+@app.route("/company/id/<id>", methods=["GET"])
+def get_company_page(id):
+    pass
+
+
+# not implemented
+@app.route("/company/create", methods=["GET", "POST"])
+def create_company():
+    pass
+
+
+# not implemented
+@app.route("/company/id/<id>/update", methods=["GET", "POST"])
+def update_company(id):
+    pass
+
+
+# not implemented
+@app.route("/company/id/<id>/delete", methods=["POST"])
+def delete_company(id):
+    pass
+
+
+# other utils
+def is_exists_user_session(id):
+    if "user_id" not in session:
+        return False
+
+    if session["user_id"] != int(id):
+        return False
+
+    return True
